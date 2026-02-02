6fad5a5cea0237fc5d59b8a9b5c5d34f36938837
Yayaa <rabiahshafira64@gmail.com>
2026-02-02 22:35:54 +0700

    let's try

---
+++ cekpH.js
@@ -0,0 +1,13 @@
+let pH = 7.5; 
+
+console.log("Mengecek nilai pH: " + pH);
+
+if (pH < 7) {
+    console.log("Sifat Larutan: Asam");
+} 
+else if (pH > 7) {
+    console.log("Sifat Larutan: Basa");
+} 
+else {
+    console.log("Sifat Larutan: Netral");
+}
